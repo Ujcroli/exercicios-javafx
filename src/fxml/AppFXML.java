b6baf552139edc440d9c66b9fe7defe9e5040a56
Ujcroli <49993264+Ujcroli@users.noreply.github.com>
2020-10-10 00:12:53 -0300

    381. Tela de Login fxml

--- src/fxml/AppFXML.java
+++ src/fxml/AppFXML.java
@@ -1,13 +1,25 @@
 package fxml;
 
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class AppFXML extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		URL arquivoFXML = getClass().getResource("/fxml/Login.fxml");
+		GridPane raix = FXMLLoader.load(arquivoFXML);
 		
+		Scene cene = new Scene(raix, 350, 400);
+		
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Tela de Login");
+		primaryStage.setScene(cene);
 		primaryStage.show();
 		
 	}
